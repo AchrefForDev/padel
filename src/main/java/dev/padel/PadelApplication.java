aa5a9e599c3ec2bea3842fbd2f1d01e12730b4ba
AchrefMahmoud <78664859+AchrefMahmoud@users.noreply.github.com>
2026-01-19 18:58:43 +0100

    JWT auth working with Spring Security 6 and Java 21

--- src/main/java/dev/padel/PadelApplication.java
+++ src/main/java/dev/padel/PadelApplication.java
@@ -1,13 +1,31 @@
 package dev.padel;
 
+import dev.padel.models.ERole;
+import dev.padel.models.Role;
+import dev.padel.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PadelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PadelApplication.class, args);
+	}
+
+
+	@Bean
+	CommandLineRunner initRoles(RoleRepository roleRepository) {
+		return args -> {
+			for (ERole role : ERole.values()) {
+				if (!roleRepository.existsByName(role)) {
+					roleRepository.save(new Role(role));
+					System.out.println("Role créé : " + role);
+				}
+			}
+		};
 	}
 
 }
---
+++ src/main/java/dev/padel/payload/request/SignupRequest.java
@@ -0,0 +1,69 @@
+package dev.padel.payload.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
+import java.util.Set;
+
+public class SignupRequest {
+
+    @Size(min = 3, max = 20)
+    private String username;
+
+    //@NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+
+    private Set<String> role;
+
+
+    //@NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+
+    // getter and setters
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<String> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<String> roles) {
+        this.role = roles;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    @Override
+    public String toString() {
+        return "SignupRequest [username=" + username  + "email=" + email
+                + ", role=" + role + ", password=" + password + "]";
+    }
+
+}
